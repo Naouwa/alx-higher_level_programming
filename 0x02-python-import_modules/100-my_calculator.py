344d444f15440b0d8c0ecafec98eb990e73f6488
Naouwa <densali.nahla@gmail.com>
2023-08-10 21:43:28 +0100

    my_calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operator.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{:d} {:s} {:d} = {:d}".format(a, sys.argv[2], b, operator[sys.argv[2]](a, b)))
