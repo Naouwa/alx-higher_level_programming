4482b7e7576a36eae61af481d98f8d079a42436a
Naouwa <densali.nahla@gmail.com>
2023-08-14 18:34:46 +0100

    6-print_matrix_integer.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = [a for a in my_string if a != 'C' and a != 'c']
+    new_string = [a for a in my_string if a != 'c' and a != 'C']
     return("".join(new_string))
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            print("{:d}".format(row[element]),
+                    end=" " if element < len(row) - 1 else "")
+        print()
