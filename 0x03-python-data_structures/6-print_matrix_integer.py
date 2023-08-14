8b711b54d80e91bb7855c50154c0b14b70c038a1
Naouwa <densali.nahla@gmail.com>
2023-08-15 00:06:58 +0100

    :'-(

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{:d}".format(row[element]),
+            print("{:d}".format(element),
                     end=" " if element < row[-1] or element > row[-1] else "")
         print()
