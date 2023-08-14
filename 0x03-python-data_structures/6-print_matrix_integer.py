3a9d18b75d0c3c420b4193ef4b9260aa914b571b
Naouwa <densali.nahla@gmail.com>
2023-08-14 23:27:02 +0100

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in range(len(row)):
-            print("{:d}".format(row[element]),
-                    end=" " if element < len(row) - 1 else "")
+            print("{:d}".format(element),
+                    end=" " if element < row[-1] or element > row[-1] else "")
         print()
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,9 @@
 def max_integer(my_list=[]):
     if my_list == 0:
         return (None)
-    my_list.sort()
-    return (my_list[-1])
-
+    else:
+        largest_i = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > largest_i:
+                largest_i = my_list[i]
+    return (largest_i)
