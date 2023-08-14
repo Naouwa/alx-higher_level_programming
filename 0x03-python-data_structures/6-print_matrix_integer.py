924b8d014df5c2df1317a24ffb74785fdde6b3b0
Naouwa <densali.nahla@gmail.com>
2023-08-14 23:55:44 +0100

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in rane(len(row)):
             print("{:d}".format(row[element]),
-                    end=" " if element != len(row) -1 else "")
-        print()
+                    end=" " if element < row[-1] or element > row[-1] else "")
+            print()
