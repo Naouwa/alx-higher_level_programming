a49cf8a2e302fb9ea37da19df9848eb34a5d2356
Naouwa <densali.nahla@gmail.com>
2023-08-14 18:59:30 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
+        for element in range(len(row)):
             print("{:d}".format(row[element]),
                     end=" " if element < len(row) - 1 else "")
         print()
