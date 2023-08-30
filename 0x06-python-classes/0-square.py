acab8625ad70c3fc383b5b901b4ac24c89dd4b1d
Naouwa <densali.nahla@gmail.com>
2023-08-30 02:56:50 +0100

    fixing

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""no module was imported"""
+"""A module containing the Square class"""
 
 class Square:
-    """ an empty class"""
+    """  This class represents a square.
+
+    It doesn't have any attributes or methods at the moment.
+    """
     pass
