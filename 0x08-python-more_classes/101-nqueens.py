1738f681a6e09194ac7f4314d141f938ef79723f
Naouwa <densali.nahla@gmail.com>
2023-09-05 00:51:48 +0100

    101-nqueens.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""The N queens puzzle
+    the challenge of placing N non-attacking queens on an N×N chessboard.
+    This is a program that solves the N queens problem.
+"""
+import sys
+
