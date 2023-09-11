87545954f24121aa98f1783bde872894b8f219dc
Naouwa <densali.nahla@gmail.com>
2023-09-11 15:14:55 +0100

    update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,8 +7,8 @@ class MyInt(int):
 
     def __eq__(self, value):
         """it iverts == operator"""
-        return self.real is not value
+        return self.real == value
 
     def __ne__(self, value):
         """it inverts != operator"""
-        return self.real is value
+        return self.real != value
