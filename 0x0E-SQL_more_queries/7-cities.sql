91e0b55bd9c4b02edaa38058b30814c936687c57
Naouwa <densali.nahla@gmail.com>
2023-10-19 03:17:09 +0100

    fix typos

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-	name VARCHAR(256) NOT NULL,
+	name VARCHAR(256) NOT NULL
 );
