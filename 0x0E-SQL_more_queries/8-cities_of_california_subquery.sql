fcb9b9d369bdae9ad489c6a9353bb45b48916afe
Nahla Densali <128868717+Naouwa@users.noreply.github.com>
2023-10-30 18:40:06 +0100

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++
@@ -1,5 +0,0 @@
--- This script lists all the cities of California that are in database hbtn_0d_usa.
--- states table contains one record (name=California)
--- result must be ascendent by cities.id
-SELECT cities.id, name FROM cities WHERE state_id =
-(SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
