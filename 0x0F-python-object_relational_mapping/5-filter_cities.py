e76474fe628ba3e821f929c19ad42e5a4248efde
Naouwa <densali.nahla@gmail.com>
2024-01-18 23:23:57 +0100

    completing mandatory tasks

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,20 +8,21 @@ import sys
 
 if __name__ == '__main__':
     """Configuring the database"""
-    db = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(
+            host="localhost",
             port=3306,
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
-            charset='utf8'
-    )
+            charset='utf8')
 
     """Creating the connection"""
-    connect = db.cursor()
-    connect.execute("SELECT cities.name FROM cities JOIN states \
-            ON cities.state_id = states.id WHERE states.name LIKE %s \
-            ORDER BY cities.id", (sys.argv[4],))
+    con = db.cursor()
+    con.execute("SELECT * FROM `cities` as `con` \
+                 INNER JOIN `states` as `st` \
+                 ON `con`.`state_id` = `st`.`id` \
+                 ORDER BY `con`.`id`")
     cities = connect.fetchall()
-    print(", ".join(city[0] for city in cities))
+    print(", ".join([city[2] for city in cities if city[4] == sys.argv[4]]))
     connect.close()
     db.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A file that contains the class definition of a State 
+""" A file that contains the class definition of a State
 and an instance Base = declarative_base()"""
 
 from sqlalchemy import Column, Integer, String
@@ -7,10 +7,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """The state class with id and name attributes of each state"""
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True,
-            nullable=False, unique=True)
-    name =  Column(String(128), nullable=False)
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
