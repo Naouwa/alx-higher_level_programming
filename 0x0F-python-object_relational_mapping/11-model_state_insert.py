9922ec78ae0f84a8511a43dac1e9ad6f90745989
Naouwa <densali.nahla@gmail.com>
2024-01-20 01:11:05 +0100

    completing mandatory tasks

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -13,13 +13,11 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
-    Base.metadata.create_all(engine)
     session = Session()
+    Base.metadata.create_all(engine)
 
-    st = State(name='Louisiana')
-    session.add(st)
+    add_st = State(name='Louisiana')
+    session.add(add_st)
     session.commit()
-
-    add_st = session.query(State).filter(State.name == 'Louisiana').first()
     print(add_st.id)
     session.close()
