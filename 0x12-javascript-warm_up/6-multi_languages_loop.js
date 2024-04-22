e7e3762234efd2c06c2ee688c547a947cc1f19ed
Naouwa <densali.nahla@gmail.com>
2024-04-22 16:15:39 +0100

    7 tasks

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// A script that prints 3 lines: (like 1-multi_languages.js)
+
+for (const lines of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(lines);
+}
