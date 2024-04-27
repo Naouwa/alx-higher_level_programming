e7e4966e97f1543520b5cf078ee9ca50498bc818
Naouwa <densali.nahla@gmail.com>
2024-04-27 20:52:05 +0100

    fixing code

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -3,7 +3,7 @@
 the letter a from the database hbtn_0e_6_usa"""
 
 import sys
-from model_State import Base, State
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -11,11 +11,12 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
+
     del_st = session.query(State).filter(State.name.like('%a%')).all()
-    for st in del_st:
-        session.delete(delete)
+    for state in del_st:
+        session.delete(state)
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    siate = session.query(State).order_by(State.id).first()
+    state = session.query(State).order_by(State.id).first()
 
     if state:
         print("{}. {}".format(state.id, state.name))
