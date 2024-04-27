c7ed58d2f6e75e7291fd01c78b46901939831785
Naouwa <densali.nahla@gmail.com>
2024-04-27 16:50:33 +0100

    fixing modelstate

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,16 +2,19 @@
 """A  file that contains the class definition of a State and
 an instance Base = declarative_base():"""
 
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from model city import city
 
 Base = declarative_base()
 
 
 class State(Base):
-    """The state class with id and name attributes of each state"""
+    """The state class with id and name attributes of each state
+    __tablename__ (str): The name of the MySQL table to store States
+    """
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
