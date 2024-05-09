43da20445aea3b83231fc92514b545d10cd4b3ff
Naouwa <densali.nahla@gmail.com>
2024-05-09 14:25:31 +0100

    fixing bugs

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This script takes a URL as input, sends a request to that URL, and displays the size of the body of the response.
+# This script takes a URL as input, sends a request.
 
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
+curl -s "$1" | wc -c
