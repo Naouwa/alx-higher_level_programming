3bfb1598eeb2e43bff2dfc17875c7c1367dee7a4
Naouwa <densali.nahla@gmail.com>
2024-05-09 06:56:18 -0700

    second task

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends GET request to the URL and displays the body of the response
 
-curl -Ls "$1"
+if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Bash script that sends a DELETE request to the URL
+
+curl -s "$1" -X DELETE
