e14c8da52d5bdc8b03b5060b38514219f01e4c71
Naouwa <densali.nahla@gmail.com>
2024-06-04 20:48:11 +0100

    fixing bugs task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,33 +3,19 @@
 const request = require('request');
 
 // The API URL is provided as the first argument
-const apiUrl = process.argv[2];
-
-// Wedge Antilles character ID
-const WEDGE_ANTILLES_ID = 18;
+const URL = process.argv[2];
 
 // Make a GET request to fetch information about Star Wars films
-request(apiUrl, (error, response, body) => {
+request(URL, (error, response, body) => {
   if (error) {
-    console.error(error);
-    return;
+    console.log(error);
+  } else if (body) {
+    // Wedge Antilles is character ID 18 - use this ID for filtering the
+    const json = JSON.parse(body);
+    const filmsChars = json.results.filter(
+      x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+    );
+    // Print the number of films with Wedge Antilles
+    console.log(filmsChars.length);
   }
-
-  // Parse the response body as JSON
-  const filmsData = JSON.parse(body);
-
-  // Initialize a variable to count films with Wedge Antilles
-  let filmsWithWedgeAntillesCount = 0;
-
-  // Iterate through each film
-  filmsData.results.forEach(film => {
-    // Check if Wedge Antilles is among the characters in the current film
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${WEDGE_ANTILLES_ID}/`)) {
-    // Increment the count if Wedge Antilles is present in the film
-      filmsWithWedgeAntillesCount++;
-    }
-  });
-
-  // Print the number of films with Wedge Antilles
-  console.log(filmsWithWedgeAntillesCount);
 });
