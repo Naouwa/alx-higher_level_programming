cf1c068bf199593df5b006fb8bf6ca67ba36c78a
Naouwa <densali.nahla@gmail.com>
2024-06-04 21:33:34 +0100

    fixing bugs

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,24 +5,20 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  const characters = JSON.parse(body).characters;
-  printCharacters(characters);
 });
 
-function printCharacters (characters) {
-  characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
+function printCharacters (characters, index) {
+  if (index < characters.length) {
+    request(characters[index], function (error, response, body) {
+      if (!error) {
+        console.log(JSON.parse(body).name);
+        printCharacters(characters, index + 1);
       }
-      console.log(JSON.parse(body).name);
     });
-  });
+  }
 }
